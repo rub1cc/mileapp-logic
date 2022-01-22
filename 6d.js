379d6eb11af124ae96fc24ad78eda5e3c38e43de
farazaulia <raz.muhammad14@gmail.com>
2022-01-22 08:41:30 +0700

    add answer

--- 6d.js
+++ 6d.js
@@ -17,40 +17,12 @@ array_code = [
   "1.4.3.",
 ];
 
-obj = {};
-
-function merge(source, target) {
-    for (const [key, val] of Object.entries(source)) {
-      if (val !== null && typeof val === `object`) {
-        if (target[key] === undefined) {
-          target[key] = new val.__proto__.constructor();
-        }
-        merge(val, target[key]);
-      } else {
-        target[key] = val;
-      }
-    }
-    return target; // we're replacing in-situ, so this is more for chaining than anything else
-  }
-
-const pustToObj = (item) => {
-    Object.entries(item).forEach(([key, val]) => {
-        console.log("val",val)
-        if(val !== null && typeof val === `object`) {
-            if(obj[key] === undefined) {
-                obj[key] = new val.__proto__.constructor();
-            }
-            pustToObj(val)
-        } else {
-            obj[key] = val;
-        }
-    })
-};
+obj = [];
 
 array_code.map((code) => {
   let cs = code.split(".");
   cs.pop();
-  pustToObj(cs.reduceRight((all, item) => ({ [item]: all }), code))
+  obj.push(cs.reduceRight((all, item) => ({ [item]: all }), code))
 });
 
 console.log("obj ->",obj)
